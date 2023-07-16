68fa8bff6e5b83c8e61ec1e2664c3e687cf9c551
Austen Adler <agadler@austenadler.com>
2023-07-16 00:55:08 -0400

    Use cbindgen in a buildscript properly

--- sensor_watch_rs/build.rs
+++ sensor_watch_rs/build.rs
@@ -1,8 +1,13 @@
-use std::process::Command;
+extern crate cbindgen;
+
+use std::env;
 
 fn main() {
-    Command::new("cbindgen")
-        .args(["--output", "sensor_watch_rs.h"])
-        .output()
-        .unwrap();
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("bindings.h");
 }
