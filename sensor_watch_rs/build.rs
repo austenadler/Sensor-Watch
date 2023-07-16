d92352f2671bfebfd753e77de9db96aba8a895f5
Austen Adler <agadler@austenadler.com>
2023-07-16 13:08:28 -0400

    Read config in cbindgen build.rs

--- sensor_watch_rs/build.rs
+++ sensor_watch_rs/build.rs
@@ -2,12 +2,15 @@ extern crate cbindgen;
 
 use std::env;
 
+use cbindgen::Config;
+
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::Builder::new()
         .with_crate(crate_dir)
+        .with_config(Config::from_file("cbindgen.toml").unwrap())
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("bindings.h");
+        .write_to_file("sensor_watch_rs.h");
 }
