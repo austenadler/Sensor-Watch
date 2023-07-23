ddb6a6ed37274732fee6d1faed559c91777e53bf
Austen Adler <agadler@austenadler.com>
2023-07-22 21:50:15 -0400

    Test mutable context

--- sensor_watch_rs/src/hi_rust.rs
+++ sensor_watch_rs/src/hi_rust.rs
@@ -1,18 +1,26 @@
 use crate::{expose_face, face::WatchFace};
+use cstr::cstr;
 use sensor_watch_sys::{info, movement_settings_t__bindgen_ty_1, MovementEvent};
 
 #[derive(Debug)]
-struct Context;
+#[repr(C)]
+struct Context {
+    last_viewed: bool,
+    watch_face_index: uint8_t,
+}
 
 expose_face!(Context, hi_rust);
 
 impl WatchFace for Context {
     fn face_initial_setup(
         _settings: &movement_settings_t__bindgen_ty_1,
-        watch_face_index: cty::uint8_t,
+        watch_face_index: uint8_t,
     ) -> Self {
         info!("In face_initial_setup ({watch_face_index})");
-        Self {}
+        Self {
+            last_viewed: false,
+            watch_face_index,
+        }
     }
 
     fn face_activate(&mut self, _settings: &movement_settings_t__bindgen_ty_1) {
@@ -25,6 +33,12 @@ impl WatchFace for Context {
         _settings: &movement_settings_t__bindgen_ty_1,
     ) -> bool {
         info!("In face_loop {self:?} ({event:?})");
+        if self.last_viewed {
+            unsafe { watch_display_string(cstr!("HI  DANI").as_ptr().cast_mut(), 0) };
+        } else {
+            unsafe { watch_display_string(cstr!("        ").as_ptr().cast_mut(), 0) };
+        }
+        self.last_viewed = !self.last_viewed;
         false
     }
 
