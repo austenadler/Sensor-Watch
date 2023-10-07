e77c0dfe4833de6b66f22e3e87fd094beb419f5e
Austen Adler <agadler@austenadler.com>
2023-10-06 21:02:38 -0400

    Fix indent of watch face macro

--- rust/sensor_watch_rs/derive/src/lib.rs
+++ rust/sensor_watch_rs/derive/src/lib.rs
@@ -22,75 +22,75 @@ pub fn describe(input: TokenStream) -> TokenStream {
     );
 
     quote! {
-            #[no_mangle]
-            pub extern "C" fn #ident_face_setup(
-                settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
-                watch_face_index: ::cty::uint8_t,
-                context_ptr: *mut *mut ::cty::c_void,
-            ) {
-                let settings = unsafe { settings.as_mut().unwrap().bit };
+        #[no_mangle]
+        pub extern "C" fn #ident_face_setup(
+            settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
+            watch_face_index: ::cty::uint8_t,
+            context_ptr: *mut *mut ::cty::c_void,
+        ) {
+            let settings = unsafe { settings.as_mut().unwrap().bit };
 
-                if unsafe { context_ptr.as_mut().unwrap() }.is_null() {
-                    let context: &'static #ident = unsafe {
-                        *context_ptr = ::sensor_watch_rs::sys::malloc(::core::mem::size_of::<::core::mem::MaybeUninit<#ident>>()) as *mut ::core::ffi::c_void;
-                        let context = (*context_ptr as *mut ::core::mem::MaybeUninit<#ident>).as_mut().unwrap();
-                        (*context).write(<#ident as WatchFace>::face_initial_setup(settings, watch_face_index));
-                        context.assume_init_mut()
-                    };
+            if unsafe { context_ptr.as_mut().unwrap() }.is_null() {
+                let context: &'static #ident = unsafe {
+                    *context_ptr = ::sensor_watch_rs::sys::malloc(::core::mem::size_of::<::core::mem::MaybeUninit<#ident>>()) as *mut ::core::ffi::c_void;
+                    let context = (*context_ptr as *mut ::core::mem::MaybeUninit<#ident>).as_mut().unwrap();
+                    (*context).write(<#ident as WatchFace>::face_initial_setup(settings, watch_face_index));
+                    context.assume_init_mut()
+                };
 
-                    context.face_post_initial_setup();
-                } else {
-                    let context = unsafe{(*context_ptr as *mut #ident).as_mut().unwrap()};
-                    context.face_setup(settings, watch_face_index)
-                }
+                context.face_post_initial_setup();
+            } else {
+                let context = unsafe{(*context_ptr as *mut #ident).as_mut().unwrap()};
+                context.face_setup(settings, watch_face_index)
             }
+        }
 
-            #[no_mangle]
-            pub extern "C" fn #ident_face_activate(
-                settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
-                context: *mut ::cty::c_void,
-            ) {
-                info!("Called: kitchen_timer_face_activate ({context:?})");
-                let settings = unsafe { settings.as_mut().unwrap().bit };
-                let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
+        #[no_mangle]
+        pub extern "C" fn #ident_face_activate(
+            settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
+            context: *mut ::cty::c_void,
+        ) {
+            info!("Called: kitchen_timer_face_activate ({context:?})");
+            let settings = unsafe { settings.as_mut().unwrap().bit };
+            let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
 
-                context.face_activate(settings)
-            }
+            context.face_activate(settings)
+        }
 
-            #[no_mangle]
-            pub extern "C" fn #ident_face_loop(
-                event: ::sensor_watch_rs::sys::movement_event_t,
-                settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
-                context: *mut ::cty::c_void,
-            ) -> bool {
-                let event = MovementEvent::from(event);
-                let settings = unsafe { settings.as_mut().unwrap().bit };
-                let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
+        #[no_mangle]
+        pub extern "C" fn #ident_face_loop(
+            event: ::sensor_watch_rs::sys::movement_event_t,
+            settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
+            context: *mut ::cty::c_void,
+        ) -> bool {
+            let event = MovementEvent::from(event);
+            let settings = unsafe { settings.as_mut().unwrap().bit };
+            let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
 
-                context.face_loop(event, settings)
-            }
+            context.face_loop(event, settings)
+        }
 
-            #[no_mangle]
-            pub extern "C" fn #ident_face_resign(
-                settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
-                context: *mut ::cty::c_void,
-            ) {
-                let settings = unsafe { settings.as_mut().unwrap().bit };
-                let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
+        #[no_mangle]
+        pub extern "C" fn #ident_face_resign(
+            settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
+            context: *mut ::cty::c_void,
+        ) {
+            let settings = unsafe { settings.as_mut().unwrap().bit };
+            let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
 
-                context.face_resign(settings)
-            }
+            context.face_resign(settings)
+        }
 
-            #[no_mangle]
-            pub extern "C" fn #ident_face_wants_background_task(
-                settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
-                context: *mut ::cty::c_void,
-            ) -> bool {
-                let settings = unsafe { settings.as_mut().unwrap().bit };
-                let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
+        #[no_mangle]
+        pub extern "C" fn #ident_face_wants_background_task(
+            settings: *mut ::sensor_watch_rs::sys::movement_settings_t,
+            context: *mut ::cty::c_void,
+        ) -> bool {
+            let settings = unsafe { settings.as_mut().unwrap().bit };
+            let context = unsafe { ::core::mem::transmute::<_, &mut #ident>(context) };
 
-                context.face_wants_background_task(settings)
-            }
+            context.face_wants_background_task(settings)
+        }
     }.into()
 }
 
